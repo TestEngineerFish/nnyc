901500499f9862619913f3cfb543a23bc063e547
sunwu <sunwu@songshuai.com>
2019-11-07 20:22:36 +0800

    update 新的网络库引入，修改加密算法

--- YXEDU/YXEDU/DataSource/Network/New/Network/Request/YYBaseRequest.swift
+++ YXEDU/YXEDU/DataSource/Network/New/Network/Request/YYBaseRequest.swift
@@ -34,10 +34,10 @@ protocol YYBaseRequest {
 extension YYBaseRequest {
 
     public var header: [String : String] {
-        let _header = ["NNYC-TOKEN" : YXConfigure.shared()!.token!,
-                       "NNYC-REQUESTTIME" : YXConfigure.shared()!.time!,
+        let _header = ["NNYC-TOKEN" : YXConfigure.shared()?.token ?? "",
+                       "NNYC-REQUESTTIME" : YXConfigure.shared()?.time ?? "",
                        "NNYC-PLATFORM" : kPlatformValue,
-                       "NNYC-VERSION" : UIDevice().appVersion()!.replacingOccurrences(of: ".", with: "")]
+                       "NNYC-VERSION" : UIDevice().appVersion()?.replacingOccurrences(of: ".", with: "") ?? ""]
         
         return _header
     }
