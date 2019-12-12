9bea3537e3a3a9d5fcac7e2ba092e7f6cc638bbf
sunwu <sunwu@songshuai.com>
2019-12-12 14:56:50 +0800

    update 关闭growingIO

--- YXEDU/YXEDU/Modules/Review/ViewController/YXReviewViewController.swift
+++ YXEDU/YXEDU/Modules/Review/ViewController/YXReviewViewController.swift
@@ -13,10 +13,6 @@ class YXReviewViewController: YXViewController {
     
     var headerView = YXReviewHeaderView()
     
-    override var preferredStatusBarStyle: UIStatusBarStyle {
-        return .lightContent
-    }
-    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
