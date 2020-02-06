70b8fe25b0e410dd287e6b31cf67c51fdade77a7
Jake To <okjaketo@Jakes-MacBook-Pro.local>
2020-02-06 15:47:39 +0800

    错词列表加入删除、折叠功能

--- YXEDU/YXEDU/Modules/Home/Views/WordList/YXWordListHeaderView.swift
+++ YXEDU/YXEDU/Modules/Home/Views/WordList/YXWordListHeaderView.swift
@@ -10,18 +10,28 @@ import UIKit
 
 class YXWordListHeaderView: UIView {
     var editClosure: (() -> Void)?
-
+    var deleteAllWordsClosure: (() -> Void)?
+    var expandClosure: ((_: Bool) -> Void)?
+    var isExpand = false {
+        didSet {
+            if isExpand {
+                expandButton.setImage(#imageLiteral(resourceName: "expandState"), for: .normal)
+                
+            } else {
+                expandButton.setImage(#imageLiteral(resourceName: "collapsedState"), for: .normal)
+            }
+        }
+    }
+    
     @IBOutlet weak var contentView: UIView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var editButton: UIButton!
+    @IBOutlet weak var deleteButton: UIButton!
+    @IBOutlet weak var expandButton: UIButton!
     
     init() {
         super.init(frame: CGRect(x: 0, y: 0, width: screenWidth, height: 30))
         initializationFromNib()
-    }
-    
-    deinit {
-        print("ccc")
     }
     
     required init?(coder: NSCoder) {
@@ -37,5 +47,13 @@ class YXWordListHeaderView: UIView {
     
     @IBAction func edit(_ sender: Any) {
         editClosure?()
+    }
+    
+    @IBAction func deleteAll(_ sender: Any) {
+        deleteAllWordsClosure?()
+    }
+    
+    @IBAction func expand(_ sender: Any) {
+        expandClosure?(isExpand)
     }
 }
--- YXEDU/YXEDU/Modules/Home/Views/WordList/YXWordListView.swift
+++ YXEDU/YXEDU/Modules/Home/Views/WordList/YXWordListView.swift
@@ -64,6 +64,7 @@ class YXWordListView: UIView, UITableViewDelegate, UITableViewDataSource {
     }
     
     var isWrongWordList = false
+    var isExpandWrongWords = false
     var wrongWordSectionData: [[String: [YXWordModel]]]? {
         didSet {
             var wordsCount = 0
@@ -208,13 +209,35 @@ class YXWordListView: UIView, UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         guard let wrongWordSectionCount = wrongWordSectionData?.count, wrongWordSectionCount > 0, isWrongWordList else { return nil }
         let wrongWordSection = wrongWordSectionData?[section]
+        let title = wrongWordSection?.keys.first
         
         let wordListHeaderView = YXWordListHeaderView()
-        wordListHeaderView.titleLabel.text = wrongWordSection?.keys.first
+        wordListHeaderView.titleLabel.text = title
         
-        if wrongWordSection?.keys.first?.contains("熟识的单词") ?? false {
-            wordListHeaderView.editButton.isHidden = false
-            wordListHeaderView.editClosure = editClosure
+        if title?.contains("熟识的单词") ?? false {
+            wordListHeaderView.deleteButton.isHidden = false
+            wordListHeaderView.deleteAllWordsClosure = {
+                guard let wrongWords = wrongWordSection?.values.first else { return }
+                
+                let alertView = YXAlertView()
+                alertView.titleLabel.text = "提示"
+                alertView.descriptionLabel.text = "\(wrongWords.count)个单词会被移出错词本，下次不要再错了哦~"
+                alertView.rightOrCenterButton.setTitle("清除", for: .normal)
+                
+                alertView.doneClosure = { _ in
+                    self.deleteAllWrongWords(wrongWords: wrongWords)
+                }
+                
+                alertView.show()
+            }
+            
+            wordListHeaderView.expandButton.isHidden = false
+            wordListHeaderView.isExpand = isExpandWrongWords
+            wordListHeaderView.expandClosure = { [weak self] isExpand in
+                guard let self = self else { return }
+                self.isExpandWrongWords = !isExpand
+                self.tableView.reloadSections(IndexSet(arrayLiteral: section), with: .automatic)
+            }
         }
         
         return wordListHeaderView
@@ -234,108 +257,105 @@ class YXWordListView: UIView, UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if let wrongWordSectionCount = wrongWordSectionData?.count, wrongWordSectionCount > 0, isWrongWordList {
-            if let count = wrongWordSectionData?[section].values.first?.count, count > 0 {
-                return count
+        if let wrongWordSectionCount = wrongWordSectionData?.count, wrongWordSectionCount > 0, let wordsCount = wrongWordSectionData?[section].values.first?.count, isWrongWordList {
+            
+            if wrongWordSectionData?[section].keys.first?.contains("熟识的单词") ?? false {
+                if isExpandWrongWords {
+                    return wordsCount
+
+                } else {
+                    return 0
+                }
                 
             } else {
-                return 1
+                return wordsCount
             }
             
+        } else if words.count > 0 {
+            return words.count
+            
         } else {
-            if words.count > 0 {
-                return words.count
-                
-            } else {
-                return 1
-            }
+            return 1
         }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "YXWordListCell", for: indexPath) as! YXWordListCell
-        var word: YXWordModel!
-        
-        if let wrongWordSectionCount = wrongWordSectionData?.count, wrongWordSectionCount > 0, isWrongWordList {
-            if var wrongWords = wrongWordSectionData?[indexPath.section].values.first, wrongWords.count > 0 {
-                word = wrongWords[indexPath.row]
-                
-                cell.removeMaskClosure = {
-                    wrongWords[indexPath.row].hidePartOfSpeechAndMeanings = !word.hidePartOfSpeechAndMeanings
+        if ((wrongWordSectionData?.count ?? 0) > 0 && isWrongWordList) || words.count > 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "YXWordListCell", for: indexPath) as! YXWordListCell
+            var word: YXWordModel!
+
+            if let wrongWordSectionCount = wrongWordSectionData?.count, wrongWordSectionCount > 0, isWrongWordList {
+                if var wrongWords = wrongWordSectionData?[indexPath.section].values.first, wrongWords.count > 0 {
+                    word = wrongWords[indexPath.row]
                     
-                    if let key = self.wrongWordSectionData?[indexPath.section].keys.first {
-                        self.wrongWordSectionData?[indexPath.section] = [key: wrongWords]
+                    cell.removeMaskClosure = {
+                        wrongWords[indexPath.row].hidePartOfSpeechAndMeanings = !word.hidePartOfSpeechAndMeanings
+                        
+                        if let key = self.wrongWordSectionData?[indexPath.section].keys.first {
+                            self.wrongWordSectionData?[indexPath.section] = [key: wrongWords]
+                        }
+                        
+                        tableView.reloadRows(at: [indexPath], with: .none)
                     }
-                    
-                    tableView.reloadRows(at: [indexPath], with: .none)
                 }
                 
-            } else {
-                let emptyCell = tableView.dequeueReusableCell(withIdentifier: "YXWordListEmptyCell", for: indexPath) as! YXWordListEmptyCell
-                return emptyCell
-            }
-            
-        } else {
-            if words.count > 0 {
+            } else if words.count > 0 {
                 word = words[indexPath.row]
-
+                
                 cell.removeMaskClosure = {
                     self.words[indexPath.row].hidePartOfSpeechAndMeanings = !self.words[indexPath.row].hidePartOfSpeechAndMeanings
                     tableView.reloadRows(at: [indexPath], with: .none)
                 }
+            }
+            
+            cell.wordLabel.text = word.word
+
+            if let partOfSpeechAndMeanings = word.partOfSpeechAndMeanings, partOfSpeechAndMeanings.count > 0 {
+                var text = ""
+
+                for index in 0..<partOfSpeechAndMeanings.count {
+                    guard let partOfSpeech = partOfSpeechAndMeanings[index].partOfSpeech, let meaning = partOfSpeechAndMeanings[index].meaning else { continue }
+                    
+                    if index == 0 {
+                        text = partOfSpeech + meaning
+                        
+                    } else {
+                        text = text + "；" + partOfSpeech + meaning
+                    }
+                }
+                
+                cell.meaningLabel.text = text
+            }
+            
+            cell.americanPronunciation = word.americanPronunciation
+            cell.englishPronunciation = word.englishPronunciation
+            
+            if word.hidePartOfSpeechAndMeanings {
+                cell.meaningLabelMask.image = #imageLiteral(resourceName: "wordListMask")
                 
             } else {
-                let emptyCell = tableView.dequeueReusableCell(withIdentifier: "YXWordListEmptyCell", for: indexPath) as! YXWordListEmptyCell
-                return emptyCell
-            }
-        }
-        
-        cell.wordLabel.text = word.word
-
-        if let partOfSpeechAndMeanings = word.partOfSpeechAndMeanings, partOfSpeechAndMeanings.count > 0 {
-            var text = ""
-
-            for index in 0..<partOfSpeechAndMeanings.count {
-                guard let partOfSpeech = partOfSpeechAndMeanings[index].partOfSpeech, let meaning = partOfSpeechAndMeanings[index].meaning else { continue }
-                
-                if index == 0 {
-                    text = partOfSpeech + meaning
-                    
-                } else {
-                    text = text + "；" + partOfSpeech + meaning
-                }
+                cell.meaningLabelMask.image = nil
             }
             
-            cell.meaningLabel.text = text
-        }
-        
-        cell.americanPronunciation = word.americanPronunciation
-        cell.englishPronunciation = word.englishPronunciation
-        
-        if word.hidePartOfSpeechAndMeanings {
-            cell.meaningLabelMask.image = #imageLiteral(resourceName: "wordListMask")
+            cell.showWordDetailClosure = { [weak self] in
+                self?.showWordDetialClosure?(word.wordId ?? 0, word.isComplexWord ?? 0)
+            }
+                
+            return cell
             
         } else {
-            cell.meaningLabelMask.image = nil
+            let emptyCell = tableView.dequeueReusableCell(withIdentifier: "YXWordListEmptyCell") as! YXWordListEmptyCell
+            return emptyCell
         }
-        
-        cell.showWordDetailClosure = { [weak self] in
-            self?.showWordDetialClosure?(word.wordId ?? 0, word.isComplexWord ?? 0)
-        }
-            
-        return cell
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        if let wrongWordSectionCount = wrongWordSectionData?.count, wrongWordSectionCount > 0, isWrongWordList {
-            return 44
-
-        } else if words.count > 0 {
+        if ((wrongWordSectionData?.count ?? 0) > 0 && isWrongWordList) || words.count > 0 {
             return 44
 
         } else {
-//            return 356
-            return tableView.estimatedRowHeight
+            return 356
+//            return tableView.estimatedRowHeight
         }
     }
     
@@ -364,5 +384,25 @@ class YXWordListView: UIView, UITableViewDelegate, UITableViewDataSource {
         }
         
         return newWords
+    }
+    
+    private func deleteAllWrongWords(wrongWords: [YXWordModel]) {
+        var wrongWordIds: [Int] = []
+        
+        for index in 0..<wrongWords.count {
+            wrongWordIds.append(wrongWords[index].wordId ?? 0)
+        }
+        
+        guard wrongWordIds.count > 0, let wrongWordIdsData = try? JSONSerialization.data(withJSONObject: wrongWordIds, options: .prettyPrinted), let string = String(data: wrongWordIdsData, encoding: .utf8) else { return }
+        
+        let request = YXWordListRequest.deleteWrongWord(wordIds: string)
+        YYNetworkService.default.request(YYStructResponse<YXResultModel>.self, request: request, success: { (response) in
+            self.wrongWordSectionData?.remove(at: 0)
+            self.tableView.reloadData()
+            UIView().toast("已清除")
+            
+        }) { error in
+            print("❌❌❌\(error)")
+        }
     }
 }
