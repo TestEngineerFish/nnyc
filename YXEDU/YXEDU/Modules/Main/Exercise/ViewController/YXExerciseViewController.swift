2a7391f702920e299ecb5763fd4df73eb89d3db5
shatingyu <916878440@qq.com>
2020-04-28 15:01:17 +0800

    修复左滑页面卡住问题

--- YXEDU/YXEDU/Modules/Main/Exercise/ViewController/YXExerciseViewController.swift
+++ YXEDU/YXEDU/Modules/Main/Exercise/ViewController/YXExerciseViewController.swift
@@ -59,11 +59,6 @@
         self.navigationController?.interactivePopGestureRecognizer?.isEnabled = false
     }
     
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        self.navigationController?.interactivePopGestureRecognizer?.isEnabled = true
-    }
-    
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
         self.loadingView?.removeFromSuperview()
