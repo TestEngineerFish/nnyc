8f9a9decc61e85166c19b8b6e3aff7dbeea382b8
shatingyu <916878440@qq.com>
2020-05-18 11:47:36 +0800

    修复显示单词详情，未收起软键盘问题

--- YXEDU/YXEDU/Modules/Main/Exercise/View/Answer/YXListenFillAnswerView.swift
+++ YXEDU/YXEDU/Modules/Main/Exercise/View/Answer/YXListenFillAnswerView.swift
@@ -16,10 +16,11 @@ class YXListenFillAnswerView: YXBaseAnswerView {
     
     private var audioBackgroundView: UIView = UIView()
     
-    private var errorCount = 0
-    
+//    private var errorCount = 0
+
     deinit {
         NotificationCenter.default.removeObserver(self, name: YXNotification.kCloseWordDetailPage, object: nil)
+        NotificationCenter.default.removeObserver(self, name: YXNotification.kShowWordDetailPage, object: nil)
     }
     
     override func createSubviews() {
@@ -55,6 +56,7 @@ class YXListenFillAnswerView: YXBaseAnswerView {
         }
         
         NotificationCenter.default.addObserver(self, selector: #selector(closeWordDetailPage), name: YXNotification.kCloseWordDetailPage, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(showWordDetailPage), name: YXNotification.kShowWordDetailPage, object: nil)
     }
      
     override func layoutSubviews() {
@@ -124,11 +126,11 @@ class YXListenFillAnswerView: YXBaseAnswerView {
             textField.resignFirstResponder()
             answerCompletion(right: true)
         } else {
-            errorCount += 1
-            
-            if errorCount > 3 {
-                textField.resignFirstResponder()
-            }
+//            errorCount += 1
+//
+//            if errorCount > 3 {
+//                textField.resignFirstResponder()
+//            }
             answerCompletion(right: false)
             
             DispatchQueue.main.asyncAfter(deadline: .now() + 0.8) { [weak self] in
@@ -137,11 +139,15 @@ class YXListenFillAnswerView: YXBaseAnswerView {
             }
         }
     }
-    
+
+    @objc func showWordDetailPage() {
+        self.textField.resignFirstResponder()
+    }
+
     @objc func closeWordDetailPage() {
-        if errorCount > 3 {
+//        if errorCount > 3 {
             textField.becomeFirstResponder()
-        }
+//        }
     }
 
     
